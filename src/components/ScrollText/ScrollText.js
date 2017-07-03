e1ef48f2a950ecf063cbe812fcb6d5fc66a58f64
Ciara Bujanos <c.buj22@gmail.com>
2017-07-02 19:15:53 -0600

    get text scrolling

---
+++ src/components/ScrollText/ScrollText.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MovieText from './MovieText';
+
+const ScrollText = () => {
+  return (
+    <div>
+      <MovieText />
+    </div>
+  )
+}
+
+
+export default ScrollText;
