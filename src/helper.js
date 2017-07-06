a3ce8348aad66ae37b46ad5b67838ebaa3970b86
Evan Miller <evanmillerny@gmail.com>
2017-07-06 12:58:54 -0600

    add fetch for homeworld

--- src/helper.js
+++ src/helper.js
@@ -6,16 +6,27 @@ export default class PeopleData {
   }
 
   getPeople() {
-    const peopleArray =[]
-    return fetch('https://swapi.co/api/people/')
+    fetch('https://swapi.co/api/people/')
     .then(res => res.json())
     .then(data => {
-      const promises = data.results.map((i) =>
-      fetch(i.homeworld).then((res) => res.json()))
-      return Promise.all(promises).then((places) => places.map((place, i) => Object.assign(data.results[i], {'homeworld': place.name}, {'population': place.population})))
+      const unresolvedPlaces = data.results.map(person => {
+        return fetch(person.homeworld)
+        .then(res => res.json())
+      })
+      Promise.all(unresolvedPlaces)
+      .then(arrayOfHomes => {
+        return arrayOfHomes.map((place, i) => {
+          console.log(data.results);
+          Object.assign(data.results[i], { homeworld: place.name } )
+        })
+      })
     })
 
-  
+
+      // return Promise.all(promises).then((places) => places.map((place, i) => Object.assign(data.results[i], {'homeworld': place.name}, {'population': place.population})))
+
+
+
     //  const promises2 = data.results.map((i) => console.log(i))
       // fetch(i.species).then((res) => res.json()))
       // return Promise.all(promises).then((places) => places.map((place, i) => Object.assign(data.results[i], {'homeworld': place.name}, {'population': place.population})))
@@ -25,20 +36,3 @@ export default class PeopleData {
   }
 
 }
-
-
-fetch('http://localhost:3001/api/frontend-staff')
-.then((res) => res.json())
-.then((info) => {
-  const promises = info.bio.map((i) => // iterates through array and map returns anything given to it, which we're given it promises.
-  fetch(i.info).then((res) => res.json()))
-
-  //Now we're going to wait for all the promises to resolve
-  return Promise.all(promises).then((members) => members.map((person, i) => Object.assign(info.bio[i], person)))
-
-  // and assign them to the original object. To add the additional info.
-
-  // we returned the promise.all to the original fetch promise so we can:
-  .then((people) => this.setState({ staff: people }))
-  .catch((error) => console.log(error))
-})
