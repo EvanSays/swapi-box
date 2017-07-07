64c5565d1fc33ae75d3cb2007348b36f3a3356b9
Ciara Bujanos <c.buj22@gmail.com>
2017-07-06 19:41:54 -0600

    add terrain

--- src/components/Card/PlanetCard.js
+++ src/components/Card/PlanetCard.js
@@ -6,6 +6,7 @@ const PlanetCard = ( {planetInfo} ) => {
   return (
     <div className="card">
       <h1>Name: {planetInfo.name}</h1>
+      <p>Terrain: {planetInfo.terrain}</p>
       <p>Population: {planetInfo.population}</p>
       <p>Climate: {planetInfo.climate}</p>
       <p>Residents: {planetInfo.residents}</p>
